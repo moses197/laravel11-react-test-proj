856e136af22044277808a24d5a2a57966b0f487b
moses197 <leomoszeko@gmail.com>
2024-04-04 12:12:53 +0100

    thr asrfijas

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,7 +5,7 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 
-export default function Authenticated({ user, header, children }) {
+export default function AuthenticatedLayout({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
@@ -21,8 +21,20 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                            <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
+                                </NavLink>
+                                
+                                <NavLink href={route('project.index')} active={route().current('project.index')}>
+                                    Project
+                                </NavLink>
+                                
+                                <NavLink href={route('task.index')} active={route().current('task.index')}>
+                                    All Task
+                                </NavLink>
+
+                                <NavLink href={route('user.index')} active={route().current('user.index')}>
+                                    Users
                                 </NavLink>
                             </div>
                         </div>
